02bf4077e19ed03e3009bded7907a04509f4cebe
MRehanArshad <f233098@cfd.nu.edu.pk>
2024-11-22 14:31:53 +0500

    Creating a Stack for all the posts

---
+++ Mini_Instagram/Posts.cpp
@@ -0,0 +1,36 @@
+#include"Posts.h"
+using namespace std;
+
+Post::Post(string post) {
+	this->postDetail = post;
+}
+
+PostStack::PostStack() {
+	Top = nullptr;
+}
+
+bool PostStack::empty() {
+	return Top == nullptr;
+}
+
+void PostStack::push(string post) {
+	if (empty()) {
+		Top = new Post(post);
+		return;
+	}
+	Post* newNode = new Post(post);
+	newNode->next = Top;
+	Top = newNode;
+}
+
+string PostStack::top() {
+	return Top->postDetail;
+}
+void PostStack::pop() {
+	if (empty())
+		return;
+	Post* newNode = Top;
+	Top = Top->next;
+	delete newNode;
+	newNode = nullptr;
+}
--- Mini_Instagram/Posts.h
+++ Mini_Instagram/Posts.h
@@ -1,1 +1,18 @@
 #pragma once
+#include"string"
+class Post {
+public:
+	std::string postDetail;
+	Post* next;
+	Post(std::string);
+};
+
+class PostStack {
+	Post* Top;
+public:
+	PostStack();
+	void push(std::string);
+	std::string top();
+	void pop();
+	bool empty();
+};
