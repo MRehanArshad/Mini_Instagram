c17f37427afe47037637afd85c85c838fe4d7645
MFaizanRizwan <f233057@cfd.nu.edu.pk>
2024-11-28 23:02:50 +0500

    Friend Class

---
+++ Mini_Instagram/Friend.h
@@ -0,0 +1,31 @@
+#pragma once
+#include<string>
+
+class Friend_Node
+{
+public:
+	std::string name;
+	std::string status;
+	Friend_Node* next;
+	Friend_Node(std::string n, std::string s);
+
+};
+
+class Friend
+{
+private:
+	Friend_Node* matrix;
+
+public:
+	Friend();
+
+	bool addFriend(std::string name, std::string status);
+
+	bool deleteFriend(std::string n);
+
+	void displayFriends();
+
+	bool searchFriend(std::string name);
+
+	bool updateFriend(std::string, std::string, std::string);
+};
